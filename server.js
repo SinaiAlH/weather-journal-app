8c9c08d90ba4b1e39291f8df9104b15629f6ac2a
Sinai Alfaro <97054941+SinaiAlH@users.noreply.github.com>
2022-03-25 18:54:14 -0600

    Ready

--- server.js
+++ server.js
@@ -2,8 +2,14 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const { request } = require('http');
 
 // Start up an instance of app
+const app = express();
+const port = process.env.PORT || 3000;
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +17,29 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+app.use(cors({origin: true}));
+
+app.get('/hello', (req, res) => {
+    res.send("Hello word")
+  //res.send(projectData);
+});
+
+app.post('/send', (req, res) => {
+    const { temperature, date, userResponse } = req.body;
+
+   projectData = {
+    'temperature': temperature,
+    'date': date,
+    'userresponse': userResponse
+   }
+
+    res.send(projectData);
+  });
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+app.listen(port, () => {
+  console.log(`Server running at http://${port}...`);
+});
--- website/app.js
+++ website/app.js
@@ -1,5 +1,60 @@
 /* Global Variables */
+const apiHeaders = {
+    'X-RapidAPI-Host': 'community-open-weather-map.p.rapidapi.com',
+    'X-RapidAPI-Key': ''
+  };
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+const getWeather = async(url, zip, key) => {
+    const options = {
+        method: 'GET',
+    };
+
+    return fetch(`${url}?zip=${zip},us&appid=${key}`)
+        .then(response => response.json())
+        .catch(err => console.error(err));
+}
+
+const sendWeather = async(url, data) => {
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    };
+
+    return fetch(url, options)
+        .then(response => response.json())
+        .catch(err => console.error(err));
+}
+
+const writeResponse = async(temperature, date, userInput) => {
+    document.getElementById('temp').innerHTML = temperature;
+    document.getElementById('date').innerHTML = date;
+    document.getElementById('content').innerHTML = userInput;
+}
+
+document.getElementById("generate").addEventListener("click", function() {
+    const key = '348aff9092458caa76ebb77b75c060fe';
+    const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+    const zip = document.getElementById('zip').value;
+    const userResponse = document.getElementById('feelings').value;
+
+    const currentWeather = getWeather(apiUrl, zip, key).then(function (currentWeather) {    
+        const data = {
+            temperature: currentWeather.main.temp,
+            date: new Date(),
+            userResponse: userResponse,
+        };
+
+        sendWeather('http://localhost:3000/send',data).then(function(response) {   
+            writeResponse(response.temperature, new Date(response.date), response.userresponse)
+        });
+    });
+
+
+});
